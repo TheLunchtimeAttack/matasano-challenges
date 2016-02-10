70bf1448713ac5cd086b4c6f03ff52fd8d5ff5b8
Rachel Player <rachel.player.2013@live.rhul.ac.uk>
2016-02-10 15:52:56 +0000

    it works now for hex to hex

--- python/s1c2.py
+++ python/s1c2.py
@@ -1,16 +1,8 @@
 # this will be the python code for the next challenge
-
 # Write a function that takes two equal-length buffers and produces their XOR combination.
 
-#If your function works properly, then when you feed it the string:
-# 1c0111001f010100061a024b53535009181c
-#... after hex decoding, and when XOR'd against:
-# 686974207468652062756c6c277320657965
-#... should produce:
-#
-#746865206b696420646f6e277420706c6179
-
 import base64
+import converters
 
 # take in hex input, process as bytes, xor the bytes, process output as hex
 def HexToHexXOR(inputString1, inputString2):
@@ -23,12 +15,17 @@ def HexToHexXOR(inputString1, inputString2):
         # declare byte string to store the intermediate value
         bytestring = ''
         
-        # iterate 
-        for i in range(0,length1):
-            bytestring += chr(ord(base64.b16decode(inputString1[i])) ^ ord(base64.b16decode(inputString2[i])))
+        inputString1Bytes = converters.read_hex(inputString1)
+        inputString2Bytes = converters.read_hex(inputString2)
         
+        lengthNew = len(inputString1Bytes)
+        
+        # iterate over the bytes XORing as we go 
+        for i in range(0,lengthNew):
+            bytestring += chr(inputString1Bytes[i] ^ inputString2Bytes[i])
+            
         # convert byte string into appropriate format
-        # output = base64.b16encode(bytestring)    
+        output = base64.b16encode(bytestring)    
 
     else:
         print ("The input strings aren't the same length")
@@ -41,7 +38,12 @@ def HexToHexXOR(inputString1, inputString2):
 
 # take in base64 input, process as bytes, xor the bytes, process output as base64
 
+
+# testing the above functions: in particular, showing the challenge is solved
 testVector1 = "1c0111001f010100061a024b53535009181c"
 testVector2 = "686974207468652062756c6c277320657965"
-testOutput = HexToHexXOR(testVector1,testVector2)
+testOutput = HexToHexXOR(testVector1,testVector2).lower()
 print testOutput
+
+if (testOutput == '746865206b696420646f6e277420706c6179'):
+    print 'success!'
