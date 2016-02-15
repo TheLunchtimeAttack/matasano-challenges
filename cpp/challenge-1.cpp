1866df6573fd3c4ce61ac5d6f6800806d14ff263
Robert Lee <robertplee@live.com>
2016-02-15 11:13:25 +0000

    Add challenge 1 test using example from cryptopals

--- cpp/challenge-1.cpp
+++ cpp/challenge-1.cpp
@@ -6,9 +6,16 @@ using namespace std;
 int main() {
 	string hexInput("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
 	string testString = ("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+	string result;
 	MatasanoConverter m;
 	
 	m.loadString(hexInput, "hex");
-	m.test();
-	//cout << m.getBase64();
+	result = m.getBase64();
+	
+	cout << "Challenge 1: ";
+	if (result.compare(testString) == 0) {
+		cout << "PASSED" << endl << "Input: " << hexInput << endl << "Output: " << result << endl << "As Required.";
+	} else {
+		cout << "FAILED" << endl << "Input: " << hexInput << endl << "Expected: " << testString << endl << "Observed: " << result << endl;
+	}
 }
