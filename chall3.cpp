f35e718eccd9d69a89b3eb5330539d151b92230f
Alex Davidson <alex.davidson92@gmail.com>
2016-02-17 18:25:46 +0000

    chalk attempt

---
+++ chall3.cpp
@@ -0,0 +1,123 @@
+// #include "chall2.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace	std;
+
+// Takes two hex characters and returns a byte
+unsigned short hex_to_byte(string hex) {
+	unsigned short byte = 0;
+	istringstream iss(hex);
+	iss >> std::hex >> byte;
+	return byte;
+}
+
+// Function from challenge 2 but couldn't get it to work in different file.
+static string hex_to_byte_xor_to_hex(string hex, string hex1) {
+    int size = hex.length();
+    int size2 = hex1.length();
+    string ret_hex = "";
+    if (size == size2){
+
+        for (int i=0; i<size; i=i+2) {
+            unsigned short byte = 0;
+            unsigned short byte2 = 0;
+            string hex2 = hex.substr(i,2);
+            string hex3 = hex1.substr(i,2);
+            istringstream iss2(hex2);
+            istringstream iss3(hex3);
+            iss2 >> std::hex >> byte;
+            iss3 >> std::hex >> byte2;
+            unsigned short xor_byte = byte ^ byte2;
+    
+            std::stringstream stream;
+            stream << std::hex << xor_byte;
+            string res(stream.str());
+            ret_hex += res;
+        }
+    }
+    else {
+        return "0";
+    }
+    return ret_hex;
+}
+
+int main(int argc, char** argv) {
+	string encrypted_hex = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+	char e = 'e';
+	unsigned short e_byte = e - 0;
+	int len = encrypted_hex.length();
+	string *hex_array = new string[len/2];
+	short *byte_array = new short[len/2];
+	int count[16*16] = {0};
+	int max = 0;
+
+	for (int i=0; i<len; i=i+2) {
+		string two_byte_hex = encrypted_hex.substr(i,2);
+		unsigned short byte = hex_to_byte(two_byte_hex);
+		byte_array[i/2] = byte;
+	}
+	for (int i=1; i<16; i++) {
+		for (int j=1; j<16; j++) {
+			string hex; 
+			stringstream ss;
+			ss << std::hex << (int)i;
+			ss << std::hex << (int)j << endl;
+			hex = ss.str();
+			unsigned short hex_byte = hex_to_byte(hex);
+			for (int k=0; k<len/2; k++) {
+				if (hex_byte == byte_array[k]) {
+					count[((i)*16)+j]++;	
+					// cout << "i= " << i << ", j= " << j << ", count= " << count[((i-1)*16)+j] << endl;
+				}
+			}
+		}
+
+	}
+
+	unsigned short possible_char[26] = {0};
+	for (char i='A'; i<='Z'; i++) {
+		unsigned short z = i - 0;
+		possible_char[z-65] = z;
+	}
+
+	for (int j=0; j<26;j++) {
+		unsigned short poss_byte = possible_char[j];
+		unsigned short xor_byte = poss_byte ^ max;
+		bool weirdness = false;
+		// cout << xor_byte << endl;
+		stringstream hex_stream;
+		hex_stream << std::hex << xor_byte;
+		string hex_xor;
+		for (int i=0;i<len/2;i++) {
+			hex_xor += hex_stream.str();
+		}
+	
+		string ret_hex = hex_to_byte_xor_to_hex(encrypted_hex,hex_xor);
+		if (ret_hex != "0"){
+	        stringstream ss1;
+	        int newlen = ret_hex.length();
+	        for (int i=0; i<newlen; i=i+2) {
+	        	string pair_hex = ret_hex.substr(i,2);
+	        	unsigned short byte_hex = hex_to_byte(pair_hex);
+	        	// Check that all the characters are either letters, spaces or apostrophes
+	        	if (!((byte_hex >= 65 && byte_hex <= 90) || (byte_hex >= 97 && byte_hex <= 122) || byte_hex == 32 || byte_hex == 39)) {
+	        		weirdness = true;
+	        	}
+	        	ss1 << (char)byte_hex;
+	        }
+	        ss1 << endl;
+	        if (!weirdness) {
+	            cout << "Decryption: " << ss1.str();
+	            cout << "Encryption character: " << (char)possible_char[j] << endl;
+	       	}
+	    }
+	    else {
+	        cout << "The lengths of the hex strings are not equal." << endl << "Terminating..." << endl;
+	        cout << encrypted_hex.length() << endl;
+	        cout << hex_xor.length() << endl;
+	    }
+	}
+	delete [] hex_array;
+}
