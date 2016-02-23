319c9c0a215c8ebd6a7eca28726f5bddd52b697b
Robert Lee <robertplee@live.com>
2016-02-23 14:11:30 +0000

    Refactor code from challenge 3 into MatasanoUtil.  Update Challenge-3 accordingly

--- cpp/challenges/challenge-3.cpp
+++ cpp/challenges/challenge-3.cpp
@@ -16,11 +16,7 @@
 //How? Devise some method for "scoring" a piece of English plaintext. 
 //Character frequency is a good metric. Evaluate each output and choose the one with the best score.
 
-struct TestString {
-	std::string s;
-	uint8_t key;
-	uint32_t score;
-};
+
 
 std::vector<uint8_t> CreateXorVector(uint8_t xor_character, uint32_t size) {
 	std::vector<uint8_t> out;
@@ -42,16 +38,6 @@ std::string StringFromVector(std::vector<uint8_t> input_vector) {
 	return out;
 }
 
-uint8_t TestForNonText(std::string input) {
-	for (uint16_t x = 0; x < input.size(); x++) {
-		if (input[x] > 126 || input[x] < 32) {
-			return 0;
-		}
-	}
-	
-	return 1;
-}
-
 TestString CreateTestString(std::string input_string, uint8_t input_key) {
 	TestString t;
 	t.s = input_string;
@@ -59,7 +45,7 @@ TestString CreateTestString(std::string input_string, uint8_t input_key) {
 	return t;
 }
 
-std::vector<TestString> InitialFilterNonText(std::vector<uint8_t> input_bytes) {
+std::vector<TestString> TestStringVectorSetup(std::vector<uint8_t> input_bytes) {
 	std::string xor_output_string;
 	uint8_t xor_character;
 	std::vector<uint8_t> xor_vector;
@@ -69,49 +55,20 @@ std::vector<TestString> InitialFilterNonText(std::vector<uint8_t> input_bytes) {
 	xor_vector = CreateXorVector(0, input_bytes.size());
 	xor_output_bytes = XorByteVectors(input_bytes, xor_vector);
 	xor_output_string = StringFromVector(xor_output_bytes);
-	if (TestForNonText(xor_output_string) == 1) {
-		output_strings.push_back(CreateTestString(xor_output_string, xor_character));
-	}
+	output_strings.push_back(CreateTestString(xor_output_string, xor_character));
 	
 	for (xor_character = 1; xor_character != 0; xor_character++) {
 		//test all potential i's
 		xor_vector = CreateXorVector(xor_character, input_bytes.size());
 		xor_output_bytes = XorByteVectors(input_bytes, xor_vector);
 		xor_output_string = StringFromVector(xor_output_bytes);
-		if (TestForNonText(xor_output_string) == 1) {
-			output_strings.push_back(CreateTestString(xor_output_string, xor_character));
-		}
+		output_strings.push_back(CreateTestString(xor_output_string, xor_character));
 	}
 	
 	return output_strings;
 }
 
-uint8_t TestForExcessivePunctuation(std::string test_string) {
-	uint8_t punctuation_count = 0;
-	
-	for (uint16_t x = 0; x < test_string.size(); x++) {
-		if ((32 < test_string[x] && test_string[x] < 48) || (57 < test_string[x] && test_string[x] < 65) || (90 < test_string[x] && test_string[x] < 97) || (122 < test_string[x] && test_string[x] < 127)) {
-			punctuation_count++;
-		}
-		if (punctuation_count == PUNCTUATION_THRESHOLD) {
-			return 0;
-		}
-	}
-	
-	return 1;
-}
 
-std::vector<TestString> FilterExcessivePunctuation(std::vector<TestString> input_strings) {
-	std::vector<TestString> output_strings;
-	
-	for (uint8_t i = 0; i < input_strings.size(); i++) {
-		if (TestForExcessivePunctuation(input_strings[i].s) == 1) {
-			output_strings.push_back(input_strings[i]);
-		}
-	}
-	
-	return output_strings;
-}
 
 int main() {
 	std::string input_string = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
@@ -120,11 +77,13 @@ int main() {
 	
 	input_byte_vector = ByteVectorFromString(input_string, "hex");;
 	
-	potential_strings = InitialFilterNonText(input_byte_vector);
+	potential_strings = TestStringVectorSetup(input_byte_vector);
+	
+	potential_strings = FilterNonPrintable(potential_strings);
 	std::cout << "Remaining Keys (initial filter): " << potential_strings.size() << std::endl;
 	
-	potential_strings = FilterExcessivePunctuation(potential_strings);
-	std::cout << "Remaining Keys (initial filter): " << potential_strings.size() << std::endl;
+	potential_strings = FilterExcessivePunctuation(potential_strings, PUNCTUATION_THRESHOLD);
+	std::cout << "Remaining Keys (excessive punctuation filter): " << potential_strings.size() << std::endl;
 	
 	for (uint16_t i = 0; i < potential_strings.size(); i++) {
 		std::cout << "Key used: " << (unsigned) potential_strings[i].key << std::endl << potential_strings[i].s << std::endl;
