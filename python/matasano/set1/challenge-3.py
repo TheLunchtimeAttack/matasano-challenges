59a918be8113f31d8087e6e2d127a79dfddb922a
Ela Berners-Lee <elizabethbernerslee@hotmail.com>
2016-02-24 11:52:33 +0000

    fix import line

--- python/matasano/set1/challenge-3.py
+++ python/matasano/set1/challenge-3.py
@@ -1,4 +1,4 @@
-import matasano.util.converters
+import matasano.util.converters as converters
 input1='1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 
 Bytes=converters.read_hex(input1)
