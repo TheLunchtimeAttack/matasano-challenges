7880a008a0387af9ec173306354b49c9d46d23f3
gchers <g.chers@gmail.com>
2016-02-26 19:13:18 +0000

    Change byte string convention
    A string of bytes is now represented as a string, where each
    character is a byte (was: list of 8-bit integers).
    Replacements:
    - read_hex() is replaced with hex_to_bytestr().
    - bytes_to_hex_string() is replaced with bytestr_to_hex().
    The following functions were removed as unnecesary (they
    were only used by the replaced functions.:
    - hex_to_byte(), combine_hex(), to_hex(), bytes_to_char.

--- python/matasano/util/converters.py
+++ python/matasano/util/converters.py
@@ -1,52 +1,17 @@
-def hex_to_byte(charin):
-    """
-    Convert an ASCII encoded hex character into it's corresponding 4 bit hex number.
-    
-    :param charIn: a string containing one character
-    :return: a 4 bit integer
-    """
-    assert type(charin) == str
-    assert len(charin) == 1
-    charin=ord(charin) # converts charIn to its `integer' value (the ascii encoding value)
-    if 48<= charin and charin <=57: # converts 0-9 characters to integers
-        charin=charin-48
-    elif 65<= charin and charin <=70: # converts uppercase to integers
-        charin=charin-55
-    elif 97<= charin and charin<=102: # converts lowercase to integers
-        charin=charin-87
-    else:
-        raise ValueError('Invalid hex character')
-    return charin
+import binascii
 
-def combine_hex(msb, lsb):
+def hex_to_bytestr(hex_string):
     """
-    Combines two 4 bit numbers into one 8 bit number
-    e.g. combine_hex(4,8) would return 0x48
-    
-    :param msb: a 4 bit integer
-    :param lsb: a 4 bit integer
-    :return: an 8 bit integer
+    Returns a byte string from its hexadecimal expression.
     """
-    assert type(msb) == int
-    assert type(lsb) == int
-    msb=msb<<4 # puts the significant bits on the left
-    output=msb | lsb # or of first and second
-    return output
+    return binascii.unhexlify(hex_string)
 
-def read_hex(hexstring):
+def bytestr_to_hex(byte_string):
     """
-    Creates a list of 8 bit numbers from a hex string
-    
-    :param hexstring: a string of ASCII encoded characters
-    :return: a list of 8 bit numbers
+    Returns the hexadecimal representation (string)
+    of a binary string.
     """
-    assert type(hexstring) == str
-    byteVals=[]
-    if len(hexstring)%2!=0:
-        raise ValueError('read_hex: Odd number of hex characters input.')
-    for i in range(0,len(hexstring)-1,2):
-        byteVals.append(combine_hex(hex_to_byte(hexstring[i]),hex_to_byte(hexstring[i+1])))
-    return byteVals
+    return binascii.unhexlify(byte_string)
 
 def base64_splitter(eightbitnumbers): 
     """
@@ -115,53 +80,6 @@ def bytes_to_base64(eightbitnumbers):
     elif length%3==2:
         outputstring = outputstring[:len(outputstring) - 1] + '='
     return outputstring
-
-def to_hex(inputnum):
-    """
-    A function that takes a 4 bit number as input and returns a one character string of the hex character representing that number
-    
-    :param inputnum: an 8 bit integer
-    :return: a string of length 1
-    """
-    assert type(inputnum) == int
-    
-    if 0 <= inputnum and inputnum <= 9:
-        return chr(inputnum + 48)
-    elif 10 <= inputnum and inputnum <= 15:
-        return chr(inputnum + 87)
-    else:
-        raise ValueError('to_hex: Invalid hex character')
-
-def bytes_to_hex_string(eightbitnumbers):
-    """
-    Converts a list of 8 bit numbers into an ASCII encoded hex string
-    
-    :param eightbitnumbers: a list of integers
-    :return: a string of ASCII characters
-    """
-    assert type(eightbitnumbers) == list
-    for x in eightbitnumbers:
-        assert type(x) == int
-    
-    outputstring = ""
-    
-    for x in range(len(eightbitnumbers)):
-        outputstring += to_hex(eightbitnumbers[x] >> 4) #select 4 msb from eightbitnumbers[x]
-        outputstring += to_hex(eightbitnumbers[x] & 0xF) #select 4 lsb from eightbitnumbers[x]
-    
-    return outputstring
-
-def bytes_to_char(byteinput):
-    """
-    Converts a list of bytes into a list of ASCII characters
-
-    param byteinput: a list of bytes
-    return a string of ASCII characters
-    """
-    chars=''
-    for x in byteinput:
-        chars+=chr(x)
-    return chars
 
 def valid_character(byteinput):
     """
