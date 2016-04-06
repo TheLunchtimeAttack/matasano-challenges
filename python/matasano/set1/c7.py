ae6cbd3b966a8cb48cd41dab2b9fe22f97ac8a29
Sam Scott <sam.scott89@gmail.com>
2016-04-06 11:49:14 +0100

    Add solution for challenge 7.

---
+++ python/matasano/set1/c7.py
@@ -0,0 +1,21 @@
+from matasano.util.converters import base64_to_bytes
+from Crypto.Cipher import AES
+import base64
+
+if __name__ == "__main__":
+
+    chal_file = open("matasano/data/c7.txt", 'r');
+
+    key = "YELLOW SUBMARINE"
+
+    # Instantiate the cipher
+    cipher = AES.new(key, AES.MODE_ECB)
+
+    # Covert from base64 to bytes
+    ct = base64_to_bytes(chal_file.read())
+
+    # Perform the decryption
+    pt = cipher.decrypt(ct)
+    print(pt.decode())
+
+
