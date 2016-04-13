95254739b2000fa36da77627eccf295192b212af
Sam Scott <sam.scott89@gmail.com>
2016-04-13 12:21:49 +0100

    Minor tweaks.

--- python/matasano/set1/c8.py
+++ python/matasano/set1/c8.py
@@ -2,7 +2,7 @@ from matasano.util.converters import hex_to_bytestr
 
 if __name__ == "__main__":
 
-    chal_file = open("matasano/data/c8.txt", 'r');
+    chal_file = open("matasano/data/c8.txt", 'r')
 
     coll_count = {}
     for idx, line in enumerate(chal_file):
@@ -11,7 +11,7 @@ if __name__ == "__main__":
         for i in range(0, len(ct), 32):
             for j in range(i+32, len(ct), 32):
                 if ct[i:i+32] == ct[j:j+32]:
-                    print("Block collision found in ciphertext: {} Block {} == Block {}".format(idx, i//16, j//16))
+                    print("Block collision found in ciphertext: {} Block {} == Block {}".format(idx, i//32, j//32))
                     count += 1
         coll_count[idx] = count
 
